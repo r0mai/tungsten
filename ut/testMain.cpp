a4a296ffb83ff462676b9fb96c1caeeadf296c67
R0mai <r0maikx02b@gmail.com>
2012-10-30 12:04:52 +0100

    Set up boost unit test framework

---
+++ ut/testMain.cpp
@@ -0,0 +1,12 @@
+
+
+#define BOOST_TEST_MODULE tungstenTest
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE( testTest )
+
+BOOST_AUTO_TEST_CASE( xxx ) {
+	BOOST_CHECK_EQUAL( 0, 0 );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
