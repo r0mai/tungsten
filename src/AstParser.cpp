ed88c03711e5620018ce09ef659d83f597dab248
R0mai <r0maikx02b@gmail.com>
2012-12-01 17:08:56 +0100

    Removed comment

--- src/AstParser.cpp
+++ src/AstParser.cpp
@@ -1,8 +1,37 @@
 
 #include "AstParser.hpp"
 
+//#include <boost/phoenix/function.hpp>
+//#include <boost/spirit/include/qi.hpp>
+
 namespace tungsten {
 
+//struct MakeInt {
+//	template <typename T>
+//	struct result { typedef AstNode type; };
+//
+//	AstNode operator()(int x) const
+//	{
+//		return AstNode::makeRational(x);
+//	}
+//};
+//
+//boost::phoenix::function<MakeInt> makeInt;
+//
+//template<class Iterator>
+//struct TungstenGrammar : boost::spirit::qi::grammar<Iterator, AstNode(), boost::spirit::ascii::space_type> {
+//
+//	namespace qi = boost::spirit::qi;
+//	namespace ascii = boost::spirit::ascii;
+//	namespace phx = boost::phoenix;
+//
+//	TungstenGrammar() : TungstenGrammar::base_type(primary) {
+//		primary = qi::int_[ qi::_val = makeInt(qi::_1) ];
+//	}
+//
+//	qi::rule<Iterator, AstNode(), ascii::blank_type> primary;
+//
+//};
 
 boost::optional<AstNode> parseInput(const std::string& input) {
 	return boost::optional<AstNode>{};
