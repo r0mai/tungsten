69e8eda20e653bbfc3232741084af5b0128034a4
R0mai <r0maikx02b@gmail.com>
2012-12-09 19:46:48 +0100

    Added make_unique to utilities

---
+++ src/util/make_unique.hpp
@@ -0,0 +1,12 @@
+#ifndef UTIL_MAKE_UNIQUE_HPP_
+#define UTIL_MAKE_UNIQUE_HPP_
+
+#include <memory>
+#include <utility>
+
+template<typename T, typename ...Args>
+std::unique_ptr<T> make_unique( Args&& ...args ) {
+    return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
+}
+
+#endif /* UTIL_MAKE_UNIQUE_HPP_ */
