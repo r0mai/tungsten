938381a9c2f133fce481dcb66b83a71b24604f89
R0mai <r0maikx02b@gmail.com>
2012-12-16 18:37:02 +0100

    Added basic test program in bin

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,3 +1,21 @@
 
+#include <iostream>
+#include <string>
 
-int main() {}
+#include "ast/Parser.hpp"
+
+int main() {
+
+	std::string line;
+
+	while ( std::getline(std::cin, line) ) {
+		auto res = tungsten::ast::parseInput( line );
+
+		if ( res ) {
+			std::cout << *res << std::endl;
+		} else {
+			std::cout << "parse error" << std::endl;
+		}
+	}
+
+}
