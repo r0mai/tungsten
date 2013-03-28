3790a4258ed90ee9bdf20f650d121b66c632396b
Phil Cassidy <vemtaai@inf.elte.hu>
2013-03-28 09:12:33 +0100

    I srsly need to stop doing this..

--- src/io/graphics/Graphics.cpp
+++ src/io/graphics/Graphics.cpp
@@ -63,7 +63,7 @@ std::string GraphicsObject::toSVGString() const {
 		R"phi(<marker id="arrow" viewbox="0 0 10 10" refX="0" refY="5" markerUnits="strokeWidth" markerwidth="3" markerheight="10" orient="auto">)phi";
 	_output<<
 		R"phi(<path d="M 0 0 L 10 5 L 0 10 z" stroke="black"/></marker>)phi";
-	_output<<"\n</defs>\n"
+	_output<<"\n</defs>\n";
 	for(const auto& shape : shapes){
 		_output<<shape->toSVGString()<<std::endl;
 	}
