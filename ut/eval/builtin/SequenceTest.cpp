0ee01dcb363160d905bb97cd995d19fbb167b236
R0mai <r0maikx02b@gmail.com>
2013-03-29 19:40:59 +0100

    Added tests for Sequence[] and SequenceHold attribute

--- ut/eval/builtin/SequenceTest.cpp
+++ ut/eval/builtin/SequenceTest.cpp
@@ -7,96 +7,195 @@ BOOST_AUTO_TEST_SUITE( SequenceTest )
 
 using namespace tungsten;
 
-BOOST_FIXTURE_TEST_CASE( Sequence_doesnt_get_evaluated_if_its_on_top_with_no_parameters, BuiltinFunctionFixture ) {
-	boost::optional<ast::Node> result = parseAndEvaluate("Sequence[]");
+
+BOOST_FIXTURE_TEST_CASE( test_f_of_Sequence_of_x_and_y , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("f[Sequence[x, y]]");
 
 	BOOST_REQUIRE( result );
 
-	BOOST_CHECK_EQUAL( *result, ast::Node::make<ast::FunctionCall>("Sequence") );
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::Identifier>("y")}) );
 }
 
 
-BOOST_FIXTURE_TEST_CASE( Sequence_doesnt_get_evaluated_if_its_on_top_with_parameters, BuiltinFunctionFixture ) {
-	boost::optional<ast::Node> result = parseAndEvaluate("Sequence[x,y]");
+BOOST_FIXTURE_TEST_CASE( test_f_of_Sequence_of_x , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("f[Sequence[x]]");
 
 	BOOST_REQUIRE( result );
 
-	BOOST_CHECK_EQUAL( *result, ast::Node::make<ast::FunctionCall>("Sequence", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::Identifier>("y")} ) );
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x")}) );
 }
 
 
-BOOST_FIXTURE_TEST_CASE( Sequence_as_the_only_parameter_to_a_function, BuiltinFunctionFixture ) {
-	boost::optional<ast::Node> result = parseAndEvaluate("f[Sequence[x,y]]");
-
-	BOOST_REQUIRE( result );
-
-	BOOST_CHECK_EQUAL( *result, ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::Identifier>("y")} ) );
-}
-
-
-BOOST_FIXTURE_TEST_CASE( Sequence_as_a_prefix_to_function_parameters, BuiltinFunctionFixture ) {
-	boost::optional<ast::Node> result = parseAndEvaluate("f[Sequence[x,y], z]");
-
-	BOOST_REQUIRE( result );
-
-	BOOST_CHECK_EQUAL( *result, ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::Identifier>("y"), ast::Node::make<ast::Identifier>("z")} ) );
-}
-
-
-BOOST_FIXTURE_TEST_CASE( Sequence_as_an_infix_to_function_parameters, BuiltinFunctionFixture ) {
-	boost::optional<ast::Node> result = parseAndEvaluate("f[a, Sequence[x,y], z]");
-
-	BOOST_REQUIRE( result );
-
-	BOOST_CHECK_EQUAL( *result, ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("a"), ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::Identifier>("y"), ast::Node::make<ast::Identifier>("z")} ) );
-}
-
-
-BOOST_FIXTURE_TEST_CASE( Sequence_as_a_postfix_to_function_parameters, BuiltinFunctionFixture ) {
-	boost::optional<ast::Node> result = parseAndEvaluate("f[x, Sequence[y, z]]");
-
-	BOOST_REQUIRE( result );
-
-	BOOST_CHECK_EQUAL( *result, ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::Identifier>("y"), ast::Node::make<ast::Identifier>("z")} ) );
-}
-
-
-BOOST_FIXTURE_TEST_CASE( empty_Sequence_as_the_only_parameter_to_a_function, BuiltinFunctionFixture ) {
+BOOST_FIXTURE_TEST_CASE( test_f_of_Sequence_of_no_arguments , BuiltinFunctionFixture ) {
 	boost::optional<ast::Node> result = parseAndEvaluate("f[Sequence[]]");
 
 	BOOST_REQUIRE( result );
 
-	BOOST_CHECK_EQUAL( *result, ast::Node::make<ast::FunctionCall>("f") );
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {}) );
 }
 
 
-BOOST_FIXTURE_TEST_CASE( empty_Sequence_as_a_prefix_to_function_parameters, BuiltinFunctionFixture ) {
-	boost::optional<ast::Node> result = parseAndEvaluate("f[Sequence[], z]");
+BOOST_FIXTURE_TEST_CASE( test_f_of_Sequence_of_no_arguments_and_x , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("f[Sequence[], x]");
 
 	BOOST_REQUIRE( result );
 
-	BOOST_CHECK_EQUAL( *result, ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("z")} ) );
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x")}) );
 }
 
 
-BOOST_FIXTURE_TEST_CASE( empty_Sequence_as_an_infix_to_function_parameters, BuiltinFunctionFixture ) {
-	boost::optional<ast::Node> result = parseAndEvaluate("f[a, Sequence[], z]");
-
-	BOOST_REQUIRE( result );
-
-	BOOST_CHECK_EQUAL( *result, ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("a"), ast::Node::make<ast::Identifier>("z")} ) );
-}
-
-
-BOOST_FIXTURE_TEST_CASE( empty_Sequence_as_a_postfix_to_function_parameters, BuiltinFunctionFixture ) {
+BOOST_FIXTURE_TEST_CASE( test_f_of_x_and_Sequence_of_no_arguments , BuiltinFunctionFixture ) {
 	boost::optional<ast::Node> result = parseAndEvaluate("f[x, Sequence[]]");
 
 	BOOST_REQUIRE( result );
 
-	BOOST_CHECK_EQUAL( *result, ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x")} ) );
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x")}) );
 }
 
 
+BOOST_FIXTURE_TEST_CASE( test_f_of_x__Sequence_of_y_and_z , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("f[x, Sequence[y], z]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::Identifier>("y"), ast::Node::make<ast::Identifier>("z")}) );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_f_of_x__Sequence_of_y_and_z_and_w , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("f[x, Sequence[y, z], w]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::Identifier>("y"), ast::Node::make<ast::Identifier>("z"), ast::Node::make<ast::Identifier>("w")}) );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_f_of_x_and_Sequence_of_y_and_z , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("f[x, Sequence[y, z]]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::Identifier>("y"), ast::Node::make<ast::Identifier>("z")}) );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_f_of_Sequence_of_x_and_y_and_z , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("f[Sequence[x, y], z]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::Identifier>("y"), ast::Node::make<ast::Identifier>("z")}) );
+}
+
+
+
+BOOST_FIXTURE_TEST_CASE( test_f_of_Sequence_of_x_and_y_with_SequenceHold, BuiltinFunctionFixture ) {
+
+	addAttribute("f", "SequenceHold");
+
+	boost::optional<ast::Node> result = parseAndEvaluate("f[Sequence[x, y]]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::FunctionCall>("Sequence", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::Identifier>("y")})}) );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_f_of_Sequence_of_x_with_SequenceHold, BuiltinFunctionFixture ) {
+
+	addAttribute("f", "SequenceHold");
+
+	boost::optional<ast::Node> result = parseAndEvaluate("f[Sequence[x]]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::FunctionCall>("Sequence", {ast::Node::make<ast::Identifier>("x")})}) );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_f_of_Sequence_of_no_arguments_with_SequenceHold, BuiltinFunctionFixture ) {
+
+	addAttribute("f", "SequenceHold");
+
+	boost::optional<ast::Node> result = parseAndEvaluate("f[Sequence[]]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::FunctionCall>("Sequence", {})}) );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_f_of_Sequence_of_no_arguments_and_x_with_SequenceHold, BuiltinFunctionFixture ) {
+
+	addAttribute("f", "SequenceHold");
+
+	boost::optional<ast::Node> result = parseAndEvaluate("f[Sequence[], x]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::FunctionCall>("Sequence", {}), ast::Node::make<ast::Identifier>("x")}) );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_f_of_x_and_Sequence_of_no_arguments_with_SequenceHold, BuiltinFunctionFixture ) {
+
+	addAttribute("f", "SequenceHold");
+
+	boost::optional<ast::Node> result = parseAndEvaluate("f[x, Sequence[]]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::FunctionCall>("Sequence", {})}) );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_f_of_x__Sequence_of_y_and_z_with_SequenceHold, BuiltinFunctionFixture ) {
+
+	addAttribute("f", "SequenceHold");
+
+	boost::optional<ast::Node> result = parseAndEvaluate("f[x, Sequence[y], z]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::FunctionCall>("Sequence", {ast::Node::make<ast::Identifier>("y")}), ast::Node::make<ast::Identifier>("z")}) );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_f_of_x__Sequence_of_y_and_z_and_w_with_SequenceHold, BuiltinFunctionFixture ) {
+
+	addAttribute("f", "SequenceHold");
+
+	boost::optional<ast::Node> result = parseAndEvaluate("f[x, Sequence[y, z], w]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::FunctionCall>("Sequence", {ast::Node::make<ast::Identifier>("y"), ast::Node::make<ast::Identifier>("z")}), ast::Node::make<ast::Identifier>("w")}) );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_f_of_x_and_Sequence_of_y_and_z_with_SequenceHold, BuiltinFunctionFixture ) {
+
+	addAttribute("f", "SequenceHold");
+
+	boost::optional<ast::Node> result = parseAndEvaluate("f[x, Sequence[y, z]]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::FunctionCall>("Sequence", {ast::Node::make<ast::Identifier>("y"), ast::Node::make<ast::Identifier>("z")})}) );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_f_of_Sequence_of_x_and_y_and_z_with_SequenceHold, BuiltinFunctionFixture ) {
+
+	addAttribute("f", "SequenceHold");
+
+	boost::optional<ast::Node> result = parseAndEvaluate("f[Sequence[x, y], z]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::FunctionCall>("Sequence", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::Identifier>("y")}), ast::Node::make<ast::Identifier>("z")}) );
+}
 
 BOOST_AUTO_TEST_SUITE_END()
 
