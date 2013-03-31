c7567f8492f48b40523d8ffb5afdec83fd481164
R0mai <r0maikx02b@gmail.com>
2013-03-31 19:42:51 +0200

    Corrected NumericQ uts

--- ut/eval/builtin/NumericQTest.cpp
+++ ut/eval/builtin/NumericQTest.cpp
@@ -7,8 +7,89 @@ BOOST_AUTO_TEST_SUITE( AbsTest )
 
 using namespace tungsten;
 
-BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_3 , BuiltinFunctionFixture ) {
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_3, BuiltinFunctionFixture ) {
 	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[3]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("True") );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_Pi, BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[Pi]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("True") );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_E, BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[E]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("True") );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_Degree, BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[Degree]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("True") );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_GoldenRatio, BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[GoldenRatio]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("True") );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_EulerGamma, BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[EulerGamma]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("True") );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_Catalan, BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[Catalan]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("True") );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_I, BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[I]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("True") );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_Khinchin, BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[Khinchin]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("True") );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_Glaisher, BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[Glaisher]");
 
 	BOOST_REQUIRE( result );
 
@@ -33,58 +114,57 @@ BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_the_quantity_3_over_4 , BuiltinFunctio
 	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("True") );
 }
 
-
-BOOST_FIXTURE_TEST_CASE( test_x , BuiltinFunctionFixture ) {
-	boost::optional<ast::Node> result = parseAndEvaluate("x");
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_x , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[x]");
 
 	BOOST_REQUIRE( result );
 
-	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("x") );
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("False") );
 }
 
 
-BOOST_FIXTURE_TEST_CASE( test_square_root_of_x , BuiltinFunctionFixture ) {
-	boost::optional<ast::Node> result = parseAndEvaluate("Sqrt[x]");
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_square_root_of_x , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[Sqrt[x]]");
 
 	BOOST_REQUIRE( result );
 
-	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("Power", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<math::Rational>(1,2)}) );
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("False") );
 }
 
 
-BOOST_FIXTURE_TEST_CASE( test_a , BuiltinFunctionFixture ) {
-	boost::optional<ast::Node> result = parseAndEvaluate("\"a\"");
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_quote_a_quote , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[\"a\"]");
 
 	BOOST_REQUIRE( result );
 
-	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::String>("a") );
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("False") );
 }
 
 
-BOOST_FIXTURE_TEST_CASE( test_quote_a_quote_plus_quote_b_quote , BuiltinFunctionFixture ) {
-	boost::optional<ast::Node> result = parseAndEvaluate("\"a\" + \"b\"");
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_the_quantity_quote_a_quote_plus_quote_b_quote , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[\"a\" + \"b\"]");
 
 	BOOST_REQUIRE( result );
 
-	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("Plus", {ast::Node::make<ast::String>("a"), ast::Node::make<ast::String>("b")}) );
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("False") );
 }
 
 
-BOOST_FIXTURE_TEST_CASE( test_sine_of_x_plus_sine_of_3 , BuiltinFunctionFixture ) {
-	boost::optional<ast::Node> result = parseAndEvaluate("Sin[x] + Sin[3]");
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_the_quantity_sine_of_x_plus_sine_of_3 , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[Sin[x] + Sin[3]]");
 
 	BOOST_REQUIRE( result );
 
-	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("Plus", {ast::Node::make<ast::FunctionCall>("Sin", {ast::Node::make<ast::Identifier>("x")}), ast::Node::make<ast::FunctionCall>("Sin", {ast::Node::make<math::Rational>(3)})}) );
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("False") );
 }
 
 
-BOOST_FIXTURE_TEST_CASE( test_cosine_of_3 , BuiltinFunctionFixture ) {
-	boost::optional<ast::Node> result = parseAndEvaluate("Cos[3]");
+BOOST_FIXTURE_TEST_CASE( test_NumericQ_of_cosine_of_3 , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("NumericQ[Cos[3]]");
 
 	BOOST_REQUIRE( result );
 
-	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("Cos", {ast::Node::make<math::Rational>(3)}) );
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("True") );
 }
 
 
