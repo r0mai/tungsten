59b552cd88810194ef62092253bbaabfdac28ebb
Phil Cassidy <vemtaai@inf.elte.hu>
2013-04-05 18:08:40 +0200

    Removed dummy messages. Ohyeah!!!

--- src/eval/builtin/Relations.cpp
+++ src/eval/builtin/Relations.cpp
@@ -8,24 +8,20 @@
 namespace tungsten { namespace eval { namespace builtin {
 
 OptionalNode Less(const ast::Operands& /*operands*/, eval::SessionEnvironment& /*sessionEnvironment*/) {
-	std::cout<<"Unimplemented Less called"<<std::endl;
 	return EvaluationFailure();
 }
 
 OptionalNode LessOrEqual(const ast::Operands& /*operands*/, eval::SessionEnvironment& /*sessionEnvironment*/) {
-	std::cout<<"Unimplemented LessOrEqual called"<<std::endl;
 	return EvaluationFailure();
 }
 
 
 OptionalNode Greater(const ast::Operands& /*operands*/, eval::SessionEnvironment& /*sessionEnvironment*/) {
-	std::cout<<"Unimplemented Greater called"<<std::endl;
 	return EvaluationFailure();
 }
 
 
 OptionalNode GreaterOrEqual(const ast::Operands& /*operands*/, eval::SessionEnvironment& /*sessionEnvironment*/) {
-	std::cout<<"Unimplemented GreaterOrEqual called"<<std::endl;
 	return EvaluationFailure();
 }
 
