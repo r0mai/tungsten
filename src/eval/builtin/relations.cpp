100f4eb5203edcd200f2eaba0cfaf729bf40e120
R0mai <r0maikx02b@gmail.com>
2013-04-07 00:32:56 +0200

    Simple implementation of relational operators

--- src/eval/builtin/relations.cpp
+++ src/eval/builtin/relations.cpp
@@ -1,25 +1,64 @@
 
 #include "functions.hpp"
-#include "eval/SessionEnvironment.hpp"
 
 namespace tungsten { namespace eval { namespace builtin {
 
-OptionalNode Less(const ast::Operands& /*operands*/, eval::SessionEnvironment& /*sessionEnvironment*/) {
-	return EvaluationFailure();
+OptionalNode Less(const ast::Operands& operands, eval::SessionEnvironment& /*sessionEnvironment*/) {
+
+	for ( unsigned i = 1; i < operands.size(); ++i ) {
+		 if ( !operands[i-1].isNumeric() || !operands[i].isNumeric() ) {
+			 return ast::Node::make<ast::FunctionCall>(ids::Less, operands);
+		 }
+		 if ( operands[i-1].getNumeric() >= operands[i].getNumeric() ) {
+			 return ast::Node::make<ast::Identifier>(ids::False);
+		 }
+	}
+
+	return ast::Node::make<ast::Identifier>(ids::True);
 }
 
-OptionalNode LessEqual(const ast::Operands& /*operands*/, eval::SessionEnvironment& /*sessionEnvironment*/) {
-	return EvaluationFailure();
+OptionalNode LessEqual(const ast::Operands& operands, eval::SessionEnvironment& /*sessionEnvironment*/) {
+
+	for ( unsigned i = 1; i < operands.size(); ++i ) {
+		 if ( !operands[i-1].isNumeric() || !operands[i].isNumeric() ) {
+			 return ast::Node::make<ast::FunctionCall>(ids::Less, operands);
+		 }
+		 if ( operands[i-1].getNumeric() > operands[i].getNumeric() ) {
+			 return ast::Node::make<ast::Identifier>(ids::False);
+		 }
+	}
+
+	return ast::Node::make<ast::Identifier>(ids::True);
 }
 
 
-OptionalNode Greater(const ast::Operands& /*operands*/, eval::SessionEnvironment& /*sessionEnvironment*/) {
-	return EvaluationFailure();
+OptionalNode Greater(const ast::Operands& operands, eval::SessionEnvironment& /*sessionEnvironment*/) {
+
+	for ( unsigned i = 1; i < operands.size(); ++i ) {
+		 if ( !operands[i-1].isNumeric() || !operands[i].isNumeric() ) {
+			 return ast::Node::make<ast::FunctionCall>(ids::Less, operands);
+		 }
+		 if ( operands[i-1].getNumeric() <= operands[i].getNumeric() ) {
+			 return ast::Node::make<ast::Identifier>(ids::False);
+		 }
+	}
+
+	return ast::Node::make<ast::Identifier>(ids::True);
 }
 
 
-OptionalNode GreaterEqual(const ast::Operands& /*operands*/, eval::SessionEnvironment& /*sessionEnvironment*/) {
-	return EvaluationFailure();
+OptionalNode GreaterEqual(const ast::Operands& operands, eval::SessionEnvironment& /*sessionEnvironment*/) {
+
+	for ( unsigned i = 1; i < operands.size(); ++i ) {
+		 if ( !operands[i-1].isNumeric() || !operands[i].isNumeric() ) {
+			 return ast::Node::make<ast::FunctionCall>(ids::Less, operands);
+		 }
+		 if ( operands[i-1].getNumeric() > operands[i].getNumeric() ) {
+			 return ast::Node::make<ast::Identifier>(ids::False);
+		 }
+	}
+
+	return ast::Node::make<ast::Identifier>(ids::True);
 }
 
 
