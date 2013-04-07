520ca163e3d1087456993493f1c8d802e8b91646
R0mai <r0maikx02b@gmail.com>
2013-04-07 12:44:39 +0200

    Implemeted custom comparator for Sort[] and OrderedQ[]

--- src/eval/builtin/Sort.cpp
+++ src/eval/builtin/Sort.cpp
@@ -5,13 +5,36 @@
 
 namespace tungsten { namespace eval { namespace builtin {
 
-OptionalNode Sort(const ast::Operands& operands, eval::SessionEnvironment& sessionEnvironment) {
-	//TODO 2nd paramter (order function)
-	if ( operands.size() != 1 ) {
+struct CustomComparator {
+	CustomComparator(const ast::Node& comparatorFunction,
+			eval::SessionEnvironment& sessionEnvironment) :
+				comparatorFunction(comparatorFunction), sessionEnvironment(sessionEnvironment) {}
 
-		sessionEnvironment.raiseMessage( Message(ids::Sort, ids::argx, {
+	bool operator()(const ast::Node& lhs, const ast::Node& rhs) {
+		ast::Node compareResult = sessionEnvironment.recursiveEvaluate(
+			ast::Node::make<ast::FunctionCall>(comparatorFunction, {lhs, rhs})
+		);
+
+		if ( compareResult.is<ast::Identifier>(ids::False) ) {
+			return false;
+		}
+		//If it doesn't return a True/False value, we take it as true
+		return true;
+	}
+
+	ast::Node comparatorFunction;
+	eval::SessionEnvironment& sessionEnvironment;
+};
+
+OptionalNode Sort(const ast::Operands& operands, eval::SessionEnvironment& sessionEnvironment) {
+
+	if ( operands.size() < 1 || operands.size() > 2 ) {
+
+		sessionEnvironment.raiseMessage( Message(ids::Sort, ids::argt, {
 				ast::Node::make<ast::Identifier>( ids::Sort ),
-				ast::Node::make<math::Rational>( operands.size() )
+				ast::Node::make<math::Rational>( operands.size() ),
+				ast::Node::make<math::Rational>( 1 ),
+				ast::Node::make<math::Rational>( 2 )
 		} ));
 
 		return EvaluationFailure();
@@ -28,19 +51,27 @@ OptionalNode Sort(const ast::Operands& operands, eval::SessionEnvironment& sessi
 
 	ast::Operands resultOperands = operands[0].get<ast::FunctionCall>().getOperands();
 
-	std::sort( resultOperands.begin(), resultOperands.end(), NodeComparatorLess{} );
+	if ( operands.size() == 1 ) {
+		std::sort( resultOperands.begin(), resultOperands.end(), NodeComparatorLess{} );
+	} else {
+		assert(operands.size() == 2);
+		CustomComparator customComparator{operands[1], sessionEnvironment};
+		std::sort( resultOperands.begin(), resultOperands.end(), customComparator );
+	}
 
 	return ast::Node::make<ast::FunctionCall>( operands[0].get<ast::FunctionCall>().getFunction(), resultOperands );
 }
 
 
 OptionalNode OrderedQ(const ast::Operands& operands, eval::SessionEnvironment& sessionEnvironment) {
-	//TODO 2nd paramter (order function)
-	if ( operands.size() != 1 ) {
 
-		sessionEnvironment.raiseMessage( Message(ids::OrderedQ, ids::argx, {
+	if ( operands.size() < 1 || operands.size() > 2 ) {
+
+		sessionEnvironment.raiseMessage( Message(ids::OrderedQ, ids::argt, {
 				ast::Node::make<ast::Identifier>( ids::OrderedQ ),
-				ast::Node::make<math::Rational>( operands.size() )
+				ast::Node::make<math::Rational>( operands.size() ),
+				ast::Node::make<math::Rational>( 1 ),
+				ast::Node::make<math::Rational>( 2 )
 		} ));
 
 		return EvaluationFailure();
@@ -57,7 +88,17 @@ OptionalNode OrderedQ(const ast::Operands& operands, eval::SessionEnvironment& s
 
 	ast::Operands resultOperands = operands[0].get<ast::FunctionCall>().getOperands();
 
-	if ( std::is_sorted( resultOperands.begin(), resultOperands.end(), NodeComparatorLess{} ) ) {
+	bool result = true;
+
+	if ( operands.size() == 1 ) {
+		result = std::is_sorted( resultOperands.begin(), resultOperands.end(), NodeComparatorLess{} );
+	} else {
+		assert(operands.size() == 2);
+		CustomComparator customComparator{operands[1], sessionEnvironment};
+		result = std::is_sorted( resultOperands.begin(), resultOperands.end(), customComparator );
+	}
+
+	if ( result ) {
 		return ast::Node::make<ast::Identifier>(ids::True);
 	}
 	return ast::Node::make<ast::Identifier>(ids::False);
--- ut/eval/builtin/SortTest.cpp
+++ ut/eval/builtin/SortTest.cpp
@@ -42,6 +42,33 @@ BOOST_FIXTURE_TEST_CASE( test_sort_the_elements_of_f_of_x__sine_of_x__f_of_x_and
 	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<math::Real>(1.), ast::Node::make<math::Real>(1.5), ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::FunctionCall>("f", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::Identifier>("y")}), ast::Node::make<ast::FunctionCall>("Sin", {ast::Node::make<ast::Identifier>("x")})}) );
 }
 
+
+BOOST_FIXTURE_TEST_CASE( test_sort_the_elements_of_the_list_4__1__3__2__2_using_the_ordering_Greater , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("Sort[{4, 1, 3, 2, 2}, Greater]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("List", {ast::Node::make<math::Rational>(4), ast::Node::make<math::Rational>(3), ast::Node::make<math::Rational>(2), ast::Node::make<math::Rational>(2), ast::Node::make<math::Rational>(1)}) );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_sort_the_elements_of_the_list_4__1__3__2__2_using_the_ordering_the_pure_function_hash_is_greater_than_hash_2 , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("Sort[{4, 1, 3, 2, 2}, #1 > #2 & ]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("List", {ast::Node::make<math::Rational>(4), ast::Node::make<math::Rational>(3), ast::Node::make<math::Rational>(2), ast::Node::make<math::Rational>(2), ast::Node::make<math::Rational>(1)}) );
+}
+
+BOOST_FIXTURE_TEST_CASE( test_sort_the_elements_of_the_list_Pi__E__2__3__1__square_root_of_2_using_the_ordering_Less , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("Sort[{Pi, E, 2, 3, 1, Sqrt[2]}, Less]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::FunctionCall>("List", {ast::Node::make<math::Rational>(1), ast::Node::make<ast::FunctionCall>("Power", {ast::Node::make<math::Rational>(2), ast::Node::make<math::Rational>(1,2)}), ast::Node::make<math::Rational>(2), ast::Node::make<ast::Identifier>("E"), ast::Node::make<math::Rational>(3), ast::Node::make<ast::Identifier>("Pi")}) );
+}
+
+
 BOOST_FIXTURE_TEST_CASE( test_OrderedQ_of_the_list_x__y, BuiltinFunctionFixture ) {
 	boost::optional<ast::Node> result = parseAndEvaluate("OrderedQ[{x, y}]");
 
@@ -131,5 +158,24 @@ BOOST_FIXTURE_TEST_CASE( test_OrderedQ_of_the_list_sine_of_3__cosine_of_3, Built
 	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("False") );
 }
 
+
+BOOST_FIXTURE_TEST_CASE( test_OrderedQ_of_the_list_4__3__2__1_and_Greater , BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("OrderedQ[{4, 3, 2, 1}, Greater]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result,ast::Node::make<ast::Identifier>("True") );
+}
+
+
+BOOST_FIXTURE_TEST_CASE( test_sort_the_elements_of_the_list_x__y__x_squared__y_squared__x_plus_y_using_the_ordering_the_pure_function_OrderedQ_of_the_list_hash__hash_2, BuiltinFunctionFixture ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("Sort[{x, y, x^2, y^2, x + y}, OrderedQ[{#1, #2}] & ]");
+
+	BOOST_REQUIRE( result );
+
+	ast::Node expected = ast::Node::make<ast::FunctionCall>("List", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::FunctionCall>("Power", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<math::Rational>(2)}), ast::Node::make<ast::Identifier>("y"), ast::Node::make<ast::FunctionCall>("Power", {ast::Node::make<ast::Identifier>("y"), ast::Node::make<math::Rational>(2)}), ast::Node::make<ast::FunctionCall>("Plus", {ast::Node::make<ast::Identifier>("x"), ast::Node::make<ast::Identifier>("y")})});
+
+	BOOST_CHECK_EQUAL( *result, expected );
+}
 
 BOOST_AUTO_TEST_SUITE_END()
