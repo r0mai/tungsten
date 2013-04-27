422df5c9e9af77c2b92436552c6bd9afe4bbcb71
Phil Cassidy <vemtaai@inf.elte.hu>
2013-04-27 21:16:47 +0200

    Reimplemented Plot[] with basic tick support

--- src/eval/builtin/Plot.cpp
+++ src/eval/builtin/Plot.cpp
@@ -4,6 +4,7 @@
 #include "eval/getHead.hpp"
 #include "eval/SessionEnvironment.hpp"
 #include "eval/IterationSpecifier.hpp"
+#include "eval/numericNodeEvaluation.hpp"
 #include "eval/orderNode.hpp"
 #include "io/graphics/Primitives.hpp"
 #include "io/graphics/Graphics.hpp"
@@ -12,129 +13,162 @@
 namespace tungsten { namespace eval { namespace builtin {
 
 OptionalNode Plot(const ast::Operands& operands, eval::SessionEnvironment& sessionEnvironment) {
-//	std::cout<<"Plot"<<std::endl;
-	using eval::getHead;
-	if(operands.size()==2){
-		ast::Node tableList = sessionEnvironment.recursiveEvaluate( ast::Node::make<ast::FunctionCall>( ids::Table, operands ) );// TODO x should be identity function.
+	if(operands.size() == 2 && operands[1].isFunctionCall(ids::List)){
+		const auto& function = operands[0]; // to be called lots and lots of times.
+		const auto& rangeOperands = operands[1].get<ast::FunctionCall>().getOperands();
+		if(rangeOperands.size() == 3 && 
+			rangeOperands[0].is<ast::Identifier>() &&
+			numericNodeEvaluation(rangeOperands[1], sessionEnvironment).isNumeric() &&
+			numericNodeEvaluation(rangeOperands[2], sessionEnvironment).isNumeric() ){
+			/* range is a-ok */
+			const auto variable = rangeOperands[0]; // is an identifier.
+			const auto minX = numericNodeEvaluation(rangeOperands[1], sessionEnvironment).getNumeric();
+			const auto maxX = numericNodeEvaluation(rangeOperands[2], sessionEnvironment).getNumeric();
+			const auto distance = maxX - minX;
 
-		if(tableList.isFunctionCall(ids::List)){
-//			std::cout<<"so far, so good."<<std::endl;
-			assert(getHead(tableList) == ast::Node::make<ast::Identifier>(eval::ids::List));
-			const auto argumentList = tableList.get<ast::FunctionCall>().getOperands();
-			if(!operands[1].get<ast::FunctionCall>().getOperands()[1].isNumeric() || !operands[1].get<ast::FunctionCall>().getOperands()[2].isNumeric()){
-				return EvaluationFailure();
-			}
-			const math::Real& start = operands[1].get<ast::FunctionCall>().getOperands()[1].getNumeric();
-			const math::Real& end = operands[1].get<ast::FunctionCall>().getOperands()[2].getNumeric();
-
-			const auto newRange = [&]() -> const ast::Node {
-				if(operands[1].get<ast::FunctionCall>().getOperands().size()==4)
-					return operands[1];
-				auto copy = operands[1];
-				copy.get<ast::FunctionCall>().getOperands().push_back(
-					ast::Node::make<math::Real>((end-start)/math::Real(500.0))
-				);
-				return copy;
-			}();
-			boost::optional<eval::IterationSpecifier> iteration = eval::IterationSpecifier::fromNode(newRange, sessionEnvironment);
-			if(!iteration || !iteration->isFinite()){
-				return EvaluationFailure();
-			}
-
-			auto it = iteration->makeIterator();
-			auto previous = it.current();
-			auto lastKnownName = ast::Node::make<ast::Identifier>(iteration->getVariable());
-			ast::Node preVal, curVal;
-			if(iteration -> hasVariable()){
-				sessionEnvironment.addPattern(ast::Node::make<ast::Identifier>(iteration->getVariable()), previous);
-					preVal = sessionEnvironment.recursiveEvaluate(operands[0]);
-			}
-			// get last known value for plot variable
-			const auto lastKnownValue = sessionEnvironment.getPatternReplacement(ast::Node::make<ast::Identifier>(iteration->getVariable()));
-			std::vector<ast::Operands> functionLine; 
-			functionLine.push_back(ast::Operands());
-			const auto yRangeMultiplier = 0.5; // if 0.5, Plots will be square, increase for taller, decrease for wider.
-			bool isValid = false;
-			for(it.advance();!it.isEnd() ; previous = it.current(), it.advance(), preVal = curVal){
-				if(iteration -> hasVariable()) {
-					sessionEnvironment.addPattern( ast::Node::make<ast::Identifier>(iteration->getVariable()), it.current() );
-						curVal = sessionEnvironment.recursiveEvaluate(operands[0]);
-				}
-				if(curVal.isNumeric() && 
-						nodeLess(ast::Node::make<math::Real>(curVal.getNumeric()),ast::Node::make<math::Real>(yRangeMultiplier*(end-start))) && 
-						nodeLess(ast::Node::make<math::Real>(-yRangeMultiplier*(end-start)), ast::Node::make<math::Real>(curVal.getNumeric())) )
-				// if curval isn't stupidly big (or invalid) then add to function Line.	
-				{
-					functionLine.back().push_back(ast::Node::make<ast::FunctionCall>(eval::ids::List, {it.current(), curVal}));
-					isValid = true;
-				}
-				else
-			   	{
-					if(isValid)
-						functionLine.push_back(ast::Operands());
-					isValid = false;
-				} 
-			}
-
-			ast::Operands flatLine;
-			for(const auto lineSegment : functionLine){
-				for(const auto point : lineSegment){
-					flatLine.push_back(point);
-				}
-			}
-
-			ast::Node helperLine = ast::Node::make<ast::FunctionCall>(eval::ids::Line, {
-					ast::Node::make<ast::FunctionCall>(eval::ids::List, flatLine)
-			});
+		//	const math::Real aspectRatio;
 			
-			const auto lineHelper = io::graphics::Line().fromOperands(helperLine.get<ast::FunctionCall>().getOperands(), sessionEnvironment);
-			const auto box = lineHelper.getBoundingBox();
-			ast::Node xAxis = ast::Node::make<ast::FunctionCall>(eval::ids::Arrow, {
-						ast::Node::make<ast::FunctionCall>(eval::ids::List, {
-								ast::Node::make<ast::FunctionCall>(eval::ids::List, {
-									ast::Node::make<math::Real>(box.minX), ast::Node::make<math::Real>(0.0)
-									}), // left-most coordinate of arrow.
-								ast::Node::make<ast::FunctionCall>(eval::ids::List, {
-									ast::Node::make<math::Real>(box.maxX), ast::Node::make<math::Real>(0.0)
-									}) // right-most coordinate.
+			ast::Operands currentLine;				// List[List[x,y], ...] : need to enclose in List.
+			ast::Operands lineVector;	// List[Line[^^^], ...] : need to enclise in List with other objects.
+			
+			math::Real advancement = distance/math::Real(768); // I like wierd numbers.
+			math::Real currentX, currentY;
+			//math::Real previousX, previousY;
+
+			math::Real minY, maxY;
+			minY = 0.0;
+			maxY = 0.0;
+			bool first=true; // signifies whether this is the first valid point on the function.
+
+			for(currentX = minX; currentX <= maxX; currentX += advancement){
+				sessionEnvironment.addPattern(variable, ast::Node::make<math::Real>(currentX));
+				const auto currentYAttempt = numericNodeEvaluation(function, sessionEnvironment);
+				if(currentYAttempt.isNumeric()){
+					// continuous so far.
+					currentY = currentYAttempt.getNumeric();
+					if(!first){
+						if(maxY<currentY) maxY = currentY;
+						if(currentY<minY) minY = currentY; // minY = std::min(minY, currentY) for some reason causes NaNs.
+					} else {
+						minY = currentY;
+						maxY = currentY;
+						first = false;
+					}
+					currentLine.push_back(ast::Node::make<ast::FunctionCall>(ids::List, { 
+									ast::Node::make<math::Real>(currentX), 
+									ast::Node::make<math::Real>(currentY)  
+								}));
+				} else {
+					// break in function Line
+					if(!currentLine.empty()){
+						lineVector.push_back(
+							ast::Node::make<ast::FunctionCall>(ids::Line, {
+								ast::Node::make<ast::FunctionCall>(ids::List, currentLine)
 							})
-					});
-			ast::Node yAxis = ast::Node::make<ast::FunctionCall>(eval::ids::Arrow, {
-						ast::Node::make<ast::FunctionCall>(eval::ids::List, {
-							ast::Node::make<ast::FunctionCall>(eval::ids::List, {
-								ast::Node::make<math::Real>(0.0), ast::Node::make<math::Real>(box.minY)
-								}), // bottom most coordinate
-							ast::Node::make<ast::FunctionCall>(eval::ids::List, {
-								ast::Node::make<math::Real>(0.0), ast::Node::make<math::Real>(box.maxY)
-								}) // top most coordinate
-						})
-					});
-			
-//			sessionEnvironment.removePattern( ast::Node::make<ast::Identifier>(iteration->getVariable()) );
-//			^^ Don't remove, may not exist. //TODO Find way to query existance.
-			if(lastKnownValue)	
-				sessionEnvironment.addPattern( lastKnownName, *lastKnownValue);
-			else
-				sessionEnvironment.removePattern(lastKnownName);
-			// restore variable.
-			//
-			const auto blue = ast::Node::make<ast::Identifier>(eval::ids::Blue);
-
-			ast::Operands merged;
+						);
+						currentLine.clear();
+					}
+				}
+			} 
+			if(!currentLine.empty()){
+				lineVector.push_back(
+					ast::Node::make<ast::FunctionCall>(ids::Line, {
+						ast::Node::make<ast::FunctionCall>(ids::List, currentLine)
+					})
+				);
+				currentLine.clear();
+			}
+			// line vector is now an array of Line[]-s
+			// move it all to merged
+			ast::Operands merged(std::move(lineVector));
+			const auto xAxis =	ast::Node::make<ast::FunctionCall>(ids::Line, {
+									ast::Node::make<ast::FunctionCall>(ids::List, {
+										ast::Node::make<ast::FunctionCall>(ids::List, {
+											ast::Node::make<math::Real>(minX), ast::Node::make<math::Real>(0.0)
+										}),
+										ast::Node::make<ast::FunctionCall>(ids::List, {
+											ast::Node::make<math::Real>(maxX), ast::Node::make<math::Real>(0.0)
+										})
+									})
+								});
+			const auto yAxis =	ast::Node::make<ast::FunctionCall>(ids::Line, {
+									ast::Node::make<ast::FunctionCall>(ids::List, {
+										ast::Node::make<ast::FunctionCall>(ids::List, {
+											ast::Node::make<math::Real>(0.0), ast::Node::make<math::Real>(minY)
+										}),
+										ast::Node::make<ast::FunctionCall>(ids::List, {
+											ast::Node::make<math::Real>(0.0), ast::Node::make<math::Real>(maxY)
+										})
+									})
+								});
 			merged.push_back(xAxis);
 			merged.push_back(yAxis);
-			merged.push_back(blue);
 
-			for(const auto lineSegment : functionLine){
-				merged.push_back(ast::Node::make<ast::FunctionCall>(eval::ids::Line, {
-					ast::Node::make<ast::FunctionCall>(eval::ids::List, lineSegment)
-				}));
+			// start work on labels.
+			//
+			// 
+			//
+			const math::Real scaleX = ceil(pow(10, floor(log10(distance)))); // 
+			const math::Real lineHeight = 0.04 * (maxY - minY);
+			for(math::Real marker = ceil(minX/scaleX); marker <= floor(maxX)/scaleX; marker=ceil(marker+1)){
+				if( marker < 0.9 && marker > -0.9)
+					continue; // skip values close to 0.
+
+				const auto tickNode =	ast::Node::make<ast::FunctionCall>(eval::ids::Line, {
+											ast::Node::make<ast::FunctionCall>(eval::ids::List, {
+												ast::Node::make<ast::FunctionCall>(eval::ids::List, {
+													ast::Node::make<math::Real>(marker*scaleX), ast::Node::make<math::Real>(-lineHeight)
+												}),
+												ast::Node::make<ast::FunctionCall>(eval::ids::List, {
+													ast::Node::make<math::Real>(marker*scaleX), ast::Node::make<math::Real>(lineHeight)
+												})
+											})
+										});
+
+				merged.push_back(tickNode);
+				merged.push_back(ast::Node::make<ast::FunctionCall>(ids::Text, {
+							ast::Node::make<math::Real>(marker*scaleX),
+							ast::Node::make<ast::FunctionCall>(ids::List, {
+								ast::Node::make<math::Real>(marker*scaleX), 
+								ast::Node::make<math::Real>(0.0)
+							})
+						}));
 			}
 
-			const auto GraphicsNode = ast::Node::make<ast::FunctionCall>(eval::ids::Graphics, 
-					{ast::Node::make<ast::FunctionCall>(eval::ids::List, merged )});
+			const math::Real scaleY = ceil(pow(10, floor(log10(maxY-minY)))); // 
+			const math::Real lineWidth = 0.04 * distance;
+			for(math::Real marker = ceil(minY/scaleY); marker <= floor(maxY)/scaleY; marker=ceil(marker+1)){
+				if( marker < 0.9 && marker > -0.9)
+					continue; // skip values close to 0.
+
+				const auto tickNode =	ast::Node::make<ast::FunctionCall>(eval::ids::Line, {
+											ast::Node::make<ast::FunctionCall>(eval::ids::List, {
+												ast::Node::make<ast::FunctionCall>(eval::ids::List, {
+													ast::Node::make<math::Real>(-lineWidth), ast::Node::make<math::Real>(marker*scaleY)
+												}),
+												ast::Node::make<ast::FunctionCall>(eval::ids::List, {
+													ast::Node::make<math::Real>(lineWidth), ast::Node::make<math::Real>(marker*scaleY) 
+												})
+											})
+										});
+
+				merged.push_back(tickNode);
+				merged.push_back(ast::Node::make<ast::FunctionCall>(ids::Text, {
+							ast::Node::make<math::Real>(marker*scaleY),
+							ast::Node::make<ast::FunctionCall>(ids::List, {
+								ast::Node::make<math::Real>(0.0),
+								ast::Node::make<math::Real>(marker*scaleY)
+							})
+						}));
+			}
+
+			// merged now includes all function lines, and axes.
+			const auto GraphicsNode = ast::Node::make<ast::FunctionCall>(ids::Graphics, 
+					{ast::Node::make<ast::FunctionCall>(ids::List, merged )});
 			return GraphicsNode;
+
 		}
+		return EvaluationFailure();
 	}
 	return EvaluationFailure();
 }
