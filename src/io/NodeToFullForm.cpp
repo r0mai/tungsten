e404bdc2fa84bc786d9cb08cf762f909c0993ae2
R0mai <r0maikx02b@gmail.com>
2013-05-02 16:06:13 +0200

    Some progress in NodeToFullForm

--- src/io/NodeToFullForm.cpp
+++ src/io/NodeToFullForm.cpp
@@ -7,20 +7,38 @@
 
 namespace tungsten { namespace io {
 
-namespace karma = boost::spirit::karma;
+namespace spirit = boost::spirit;
+namespace karma = spirit::karma;
+namespace phx = boost::phoenix;
+
+void setRational(double& rational, const ast::Node& node) {
+	assert(node.is<math::Rational>());
+	rational = node.get<math::Rational>().convert_to<double>();
+}
+
+bool isRational(const ast::Node& node) {
+	return node.is<math::Rational>();
+}
 
 template<class Iterator>
 struct FullFormGrammar : karma::grammar<Iterator, ast::Node()> {
+
 	FullFormGrammar() : FullFormGrammar::base_type(start) {
 		using karma::_1;
-		using karma::char_;
+		using karma::double_;
 		using karma::_val;
-		start = char_[_1 = 'c'];
+		using karma::eps;
+		using karma::stream;
+
+		start = eps(phx::bind(&isRational, _val)) << double_[phx::bind(&setRational, _1, _val)];
+
+		functionCallRule = start[_1 = phx::bind((const ast::Node& (ast::FunctionCall::*)() const)(&ast::FunctionCall::getFunction), _val)];
+
 
 	}
-
+	
 	karma::rule<Iterator, ast::Node()> start;
-	karma::rule<Iterator, char> test;
+	karma::rule<Iterator, ast::FunctionCall()> functionCallRule; 
 };
 
 
