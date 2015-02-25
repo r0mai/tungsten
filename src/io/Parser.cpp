732d7a52f91c870756a8d163a21b954ce763bb8e
Matthias Vegh <matyas.vegh@gmail.com>
2015-02-25 20:53:41 +0100

    Defer semantic action until after lookahead succeeds

--- src/io/Parser.cpp
+++ src/io/Parser.cpp
@@ -512,7 +512,7 @@ struct TungstenGrammar : boost::spirit::qi::grammar<Iterator, ast::Node(), Skipp
 		factorialExpression =
 				applyExpression[_val = _1] >> *(
 						lit("!!")[phx::bind(&operatorFactorial2, _val)] |
-						lexeme[lit("!")[phx::bind(&operatorFactorial, _val)] >> &(!lit('=') | lit("=="))]
+						lexeme[lit("!") >> &(!lit('=') | lit("=="))][phx::bind(&operatorFactorial, _val)]
 						);
 
 		applyExpression =
