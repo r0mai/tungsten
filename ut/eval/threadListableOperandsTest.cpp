e2d55f1d72cf25c62800e991bb530bd4885652ad
Matthias Vegh <matyas.vegh@gmail.com>
2015-02-26 21:46:37 +0100

    Add leaf tests for isEvaluationThreadSafe

--- ut/eval/threadListableOperandsTest.cpp
+++ ut/eval/threadListableOperandsTest.cpp
@@ -1,6 +1,7 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include "eval/Identifiers.hpp"
 #include "eval/threadListableOperands.hpp"
 
 BOOST_AUTO_TEST_SUITE( threadListableOperandsTest )
@@ -41,5 +42,25 @@ BOOST_AUTO_TEST_CASE( threading_successful_FunctionCall_with_empty_Head_inside )
 }
 
 //TODO do more
+
+BOOST_AUTO_TEST_CASE( isEvaluationOfNodeThreadSafe_should_be_true_for_reals ) {
+	bool returnValue = eval::isEvaluationOfNodeThreadSafe(ast::Node::make<math::Real>(1));
+	BOOST_CHECK( returnValue );
+}
+
+BOOST_AUTO_TEST_CASE( isEvaluationOfNodeThreadSafe_should_be_true_for_rationals ) {
+	bool returnValue = eval::isEvaluationOfNodeThreadSafe(ast::Node::make<math::Rational>(1));
+	BOOST_CHECK( returnValue );
+}
+
+BOOST_AUTO_TEST_CASE( isEvaluationOfNodeThreadSafe_should_be_true_for_strings ) {
+	bool returnValue = eval::isEvaluationOfNodeThreadSafe(ast::Node::make<ast::String>("foobar"));
+	BOOST_CHECK( returnValue );
+}
+
+BOOST_AUTO_TEST_CASE( isEvaluationOfNodeThreadSafe_should_be_true_for_identifiers ) {
+	bool returnValue = eval::isEvaluationOfNodeThreadSafe(ast::Node::make<ast::Identifier>(eval::ids::Plus));
+	BOOST_CHECK( returnValue );
+}
 
 BOOST_AUTO_TEST_SUITE_END()
