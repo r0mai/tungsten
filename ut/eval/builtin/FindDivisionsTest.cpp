297c40937ac6ed30be3e0d8cfc7663b99fd11db2
Matthias Vegh <matyas.vegh@gmail.com>
2015-03-16 15:46:06 +0100

    Add unary and binary tests for ContinuedFraction[]

--- ut/eval/builtin/FindDivisionsTest.cpp
+++ ut/eval/builtin/FindDivisionsTest.cpp
@@ -5,12 +5,10 @@
 #include "Fixture.hpp"
 #include "ast/Identifier.hpp"
 
-BOOST_AUTO_TEST_SUITE( FindDivisionsTest )
-
 using namespace tungsten;
 using namespace eval;
 
-// FindDivisions ----
+BOOST_AUTO_TEST_SUITE( FindDivisionsTest )
 
 BOOST_FIXTURE_TEST_CASE( test_0_1_5_finddivisions, BuiltinFunctionFixture ) {
 	boost::optional<ast::Node> result = parseAndEvaluate("FindDivisions[{0,1},5]");
@@ -24,6 +22,49 @@ BOOST_FIXTURE_TEST_CASE( test_0_1_5_finddivisions, BuiltinFunctionFixture ) {
 			ast::Node::make<math::Rational>(3, 5),
 			ast::Node::make<math::Rational>(4, 5),
 			ast::Node::make<math::Rational>(1) }));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_FIXTURE_TEST_SUITE( ContinuedFractionTest, BuiltinFunctionFixture )
+
+BOOST_AUTO_TEST_CASE( test_continuedfraction_of_3_4 ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("ContinuedFraction[3.4]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result, ast::Node::make<ast::FunctionCall>(ids::List, {
+			ast::Node::make<math::Rational>(3),
+			ast::Node::make<math::Rational>(2),
+			ast::Node::make<math::Rational>(2) }));
+}
+
+BOOST_AUTO_TEST_CASE( test_continuedfraction_of_pi_with_20_elements ) {
+	boost::optional<ast::Node> result = parseAndEvaluate("ContinuedFraction[N[Pi], 20]");
+
+	BOOST_REQUIRE( result );
+
+	BOOST_CHECK_EQUAL( *result, ast::Node::make<ast::FunctionCall>(ids::List, {
+			ast::Node::make<math::Rational>(3),
+			ast::Node::make<math::Rational>(7),
+			ast::Node::make<math::Rational>(15),
+			ast::Node::make<math::Rational>(1),
+			ast::Node::make<math::Rational>(292),
+			ast::Node::make<math::Rational>(1),
+			ast::Node::make<math::Rational>(1),
+			ast::Node::make<math::Rational>(1),
+			ast::Node::make<math::Rational>(2),
+			ast::Node::make<math::Rational>(1),
+			ast::Node::make<math::Rational>(3),
+			ast::Node::make<math::Rational>(1),
+			ast::Node::make<math::Rational>(14),
+			ast::Node::make<math::Rational>(2),
+			ast::Node::make<math::Rational>(1),
+			ast::Node::make<math::Rational>(1),
+			ast::Node::make<math::Rational>(2),
+			ast::Node::make<math::Rational>(2),
+			ast::Node::make<math::Rational>(2),
+			ast::Node::make<math::Rational>(2) }));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
