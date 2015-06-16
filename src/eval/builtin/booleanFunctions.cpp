5307f0488133aee18ff293481b636a2ac4055cac
Matthias Vegh <matyas.vegh@gmail.com>
2015-06-16 21:45:18 +0200

    Return symbolic value if some elements aren't boolean

--- src/eval/builtin/booleanFunctions.cpp
+++ src/eval/builtin/booleanFunctions.cpp
@@ -3,6 +3,7 @@
 #include "eval/SessionEnvironment.hpp"
 
 #include <utility>
+#include <deque>
 
 namespace tungsten { namespace eval { namespace builtin {
 
@@ -143,20 +144,44 @@ OptionalNode AllTrue(const ast::Operands& operands,
 		return ast::Node::make<ast::Identifier>(ids::True);
 	}
 
-	std::vector<ast::Node> nonTrues;
+	std::deque<ast::Node> nonTrues;
 
 	for(const auto& element: list) {
 		auto returnValue = sessionEnvironment.recursiveEvaluate(
 				ast::Node::make<ast::FunctionCall>(predicate, {element}));
+		if(returnValue.is<ast::Identifier>() && returnValue.get<ast::Identifier>() == ids::False) {
+			return returnValue; // ids::False
+		}
 		if(!returnValue.is<ast::Identifier>() || returnValue.get<ast::Identifier>() != ids::True) {
 			nonTrues.push_back(std::move(returnValue));
 		}
 	}
 
-	if(nonTrues.empty()) {
-		return ast::Node::make<ast::Identifier>(ids::True);
+	if(!nonTrues.empty()) {
+		auto first = std::move(nonTrues.front());
+		nonTrues.pop_front();
+
+		ast::FunctionCall andFunction = ids::And;
+		andFunction.getOperands().push_back(std::move(first));
+		andFunction.getOperands().push_back(
+		    ast::Node::make<ast::Identifier>(ids::True));
+
+		for(auto& nonTrue: nonTrues) {
+			auto rightMost = andFunction.getOperands().back();
+			ast::Node newOp = ast::Node::make<ast::FunctionCall>(ids::And, {
+					std::move(nonTrue),
+					std::move(rightMost)
+			});
+			andFunction.getOperands().back() = std::move(newOp);
+			andFunction.getOperands().push_back(
+			    ast::Node::make<ast::Identifier>(ids::True));
+		}
+
+		return sessionEnvironment.recursiveEvaluate(
+		    ast::Node::make<ast::FunctionCall>(andFunction));
+
 	} else {
-		return ast::Node::make<ast::Identifier>(ids::False);
+		return ast::Node::make<ast::Identifier>(ids::True);
 	}
 }
 
